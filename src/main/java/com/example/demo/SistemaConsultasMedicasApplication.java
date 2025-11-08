3a5ff81c4be9c5268aed52a9f4fa7ad90490f847
Gabriel Monte <gabrielmonte485@gmail.com>
2025-11-08 15:45:30 -0300

    Primeiro commit com arquivos Java

---
+++ src/main/java/com/example/demo/SistemaConsultasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaConsultasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaConsultasMedicasApplication.class, args);
+	}
+
+}
